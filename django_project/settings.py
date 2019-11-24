ee887c45ec52b361416a4fc3c26dfcdf119e5822
Jason Sweeney <jsnswny@gmail.com>
2019-11-24 12:15:47 +0000

    Fixed margins of title box and adjusted margins of slide image

--- django_project/settings.py
+++ django_project/settings.py
@@ -145,7 +145,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jsnswny@gmail.com'
-EMAIL_HOST_PASSWORD = "rocknroll3r"
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True

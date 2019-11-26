9a8ceed51296a54c7508d53fbb63c6aa0cc8af74
Jason Sweeney <jsnswny@gmail.com>
2019-11-26 16:09:36 +0000

    Updated allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['themontgomerie.co.uk']
+ALLOWED_HOSTS = ['reservetable.co.uk', 'www.reservetable.co.uk']
 
 
 # Application definition
